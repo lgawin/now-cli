b9de7736974860e0250ea66a9934680af5b3cc47
Łukasz Gawin <lukasz.gawin@foqus.mobi>
2026-07-09 23:51:30 +0200

    adjust install script to get the latest release

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION="v0.0.1-alpha"
+echo "🔍 Fetching the latest version of now-cli..."
+VERSION=$(curl -s https://api.github.com/repos/lgawin/now-cli/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+if [ -z "$VERSION" ]; then
+    echo "❌ Error: Could not determine the latest release version." >&2
+    exit 1
+fi
+
 URL="https://github.com/lgawin/now-cli/releases/download/$VERSION/now-linux-x86_64.tar.gz"
 TARGET_DIR="/usr/local/bin"
+
+echo "⏳ Starting now-cli installer ($VERSION)..."
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "🔐 Installation requires administrator privileges. Requesting sudo..."
